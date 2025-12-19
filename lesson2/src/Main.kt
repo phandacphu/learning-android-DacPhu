6594e7d43d6a6372feaec7d8cd131fd199cfb352
PDACPHU\ACER <phandacphu2201@gmail.com>
2025-12-19 10:23:10 +0700

    lesson 2

--- lesson2/src/Main.kt
+++ lesson2/src/Main.kt
@@ -1,14 +1,60 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+fun baib1(){
+    val age: Int = 19
+    println(if (age >= 18) "da du tuoi" else "chua du tuoi")
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun baib2(){
+    val name: String? = "an"
+    val length = name?.length
+    println(length)
+}
+
+fun baib3(){
+    val email: String? = null
+    val length = email?.length ?: 0
+    println(length)
+}
+
+fun baib4(){
+    val score: Int = 2
+    val result = if ( score>= 5) "dau" else "rot"
+    println(result)
+}
+
+fun baib5(){
+    val phone: String? = "0123456789"
+    val length = phone?.length ?: 0
+    println(if (length >= 10) "so hop le" else "so ko hop le")
+}
+
+fun baic1(){
+    val age: Int = 19
+    println(if(age<12) "tre em" else if(age<=12 && age>=17) "thieu nien" else "nguoi lon")
+}
+
+fun baic2(){
+    val username: String? = null
+    if (username != null) {
+        print(username)
+    } else{
+        print("khach")
     }
+}
+
+fun printLength(text: String?) {
+    val length = text?.length ?: 0
+    print(length)
+}
+
+fun main() {
+    baib1()
+    baib2()
+    baib3()
+    baib4()
+    baib5()
+    baic1()
+    baic2()
+    printLength("null")
 }
