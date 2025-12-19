f635d7c87ad931284d08f90b86cb90bbd99e5025
PDACPHU\ACER <phandacphu2201@gmail.com>
2025-12-19 10:23:10 +0700

    ll

--- lesson2/src/Main.kt
+++ lesson2/src/Main.kt
@@ -9,6 +9,5 @@ fun main() {
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
     }
 }
